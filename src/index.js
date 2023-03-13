2d2cf47be98d22365a15d5c2c8e2e450aa03b293
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-13 21:16:03 +0200

    props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ const rootEl = document.getElementById('root');
 const root = ReactDOM.createRoot(rootEl);
 
 function App() {
-  return <h1>بسم الله</h1>;
+  const message = 'Enter age';
+  return (
+    <div>
+      <h1>بسم الله</h1>
+      <input
+        type={'number'}
+        style={{ backgroundColor: 'yellow' }}
+        min="6"
+        step={6}
+        alt={message}
+      ></input>
+    </div>
+  );
 }
 
 root.render(<App />);
